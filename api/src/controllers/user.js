55e612cffe36e2022c63a1abcd23ee16667595b2
snc0lt <xjinxer@gmail.com>
2020-10-01 19:54:52 -0500

    user login fixed

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,26 +1,26 @@
 const { User } = require("../db.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cloudinary = require ('cloudinary')
+const cloudinary = require('cloudinary')
 
 const SECRET = process.env.SECRET;
 
 
 
- const uploadImage = (file) => new Promise ((resolve,reject) => {
+const uploadImage = (file) => new Promise((resolve, reject) => {
 
   cloudinary.config({
-    cloud_name:'dxnd3uqlx',
-    api_key:'151315768991554',
-    api_secret:'wonkrqTdVInQ8yTjb_-yJpRYxjE'
+    cloud_name: 'dxnd3uqlx',
+    api_key: '151315768991554',
+    api_secret: 'wonkrqTdVInQ8yTjb_-yJpRYxjE'
   })
 
-  cloudinary.uploader.upload(file.tempFilePath,function(result,err){
-    if(err) {return reject(err)} 
+  cloudinary.uploader.upload(file.tempFilePath, function (result, err) {
+    if (err) { return reject(err) }
     return resolve(result.url)
   })
 
-}) 
+})
 
 const hashPassword = (password) => new Promise((resolve, reject) => {
   bcrypt.genSalt(10, (err, salt) => {
@@ -45,15 +45,15 @@ module.exports = {
 
   async getUsers(req, res) {
     try {
-        const users = await User.findAll()
-        if (users && users.length === 0) {
-              return res.status(404).send({ message: "No hay usuarios" });
-        }
-        res.status(200).send(users);            
+      const users = await User.findAll()
+      if (users && users.length === 0) {
+        return res.status(404).send({ message: "No hay usuarios" });
+      }
+      res.status(200).send(users);
     } catch (error) {
-        console.log(error)            
-    }             
-  },  
+      console.log(error)
+    }
+  },
 
   async getOneUser(req, res) {
     try {
@@ -67,16 +67,16 @@ module.exports = {
   },
 
   async registerUser(req, res) {
-    
+
     let image = 'https://www.ibm.com/blogs/systems/mx-es/wp-content/themes/ibmDigitalDesign/assets/img/anonymous.jpg'
-    
+
     const { email, name, lastName, city, country, password, admin } = req.body;
     if (!name || !lastName || !email || !password) {
       return res.status(400).send({ message: "Faltan campos obligatorios", status: 400 });
     }
 
     try {
-      if(req.files){
+      if (req.files) {
         const file = req.files.photo;
         image = await uploadImage(file)
       }
@@ -84,11 +84,11 @@ module.exports = {
       if (user) {
         return res.status(400).send({ message: "Ya existe un usuario con ese email", status: 400 });
       }
-      const userData = { email, name, lastName, password, city, country, admin,image };
+      const userData = { email, name, lastName, password, city, country, admin, image };
       const newUser = await User.create(userData)
       return res.status(201).send(newUser)
     } catch (err) {
-      console.log('err',err)
+      console.log('err', err)
       return res.status(500).send(err)
     }
 
@@ -97,20 +97,19 @@ module.exports = {
   async loginUser(req, res) {
     const { email, password } = req.body;
     try {
-      
+
       const user = await User.findOne({ where: { email: email } })
       if (!user) {
         return res.status(400).send({ message: "Cuenta inexistente, registrese", status: 400 })
-      } else {
-        const validate = bcrypt.compare(password, user.password)
-        if (!validate) return res.status(400).json({ message: 'Credenciales inválidas' })
-        const token = jwt.sign({ id: user.id }, SECRET)
-        res.header('auth-token', token)
-        res.status(200).send({ token: token, user })
       }
+      const validate = await bcrypt.compare(password, user.password )
+      if (!validate) return res.status(400).json({ message: 'Credenciales inválidas' })
+      const token = jwt.sign({ id: user.id }, SECRET)
+      res.header('auth-token', token)
+      res.status(200).send({ token: token, user })
     } catch (error) { console.log(error) }
   },
-  
+
   async promoteUser(req, res) {
     const admin = req.user;
     const user = await User.findByPk(admin.id)
@@ -131,12 +130,12 @@ module.exports = {
 
   async userEditProfile(req, res) {
     const { city, country, googleId, gitHubId } = req.body;
-    if(req.files){
+    if (req.files) {
       const file = req.files.photo;
       image = await uploadImage(file)
     }
     const user = await User.findByPk(req.params.id)
-    
+
     if (!user) return res.status(404).send('Usuario inexistente para ese id')
     user.city = city || user.city;
     user.country = country || user.country;
@@ -150,12 +149,12 @@ module.exports = {
   async getInstructors(req, res) {
     try {
       const instructors = await User.findAll({
-      where: {
-        instructor: true,
-      }
+        where: {
+          instructor: true,
+        }
       })
-      res.status(200).send(instructors) 
-    }catch (error) {
+      res.status(200).send(instructors)
+    } catch (error) {
       console.log(error)
     }
   },
@@ -163,12 +162,12 @@ module.exports = {
   async getPms(req, res) {
     try {
       const pms = await User.findAll({
-      where: {
-        pm: true,
-      }
+        where: {
+          pm: true,
+        }
       })
-      res.status(200).send(pms) 
-    }catch (error) {
+      res.status(200).send(pms)
+    } catch (error) {
       console.log(error)
     }
   }
