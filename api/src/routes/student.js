6c1ef4294488941868e6d83e0d97b27089e744e5
rodvla <67304782+rodvla@users.noreply.github.com>
2020-10-03 16:12:48 -0300

    fixes student model-controller

--- api/src/routes/student.js
+++ api/src/routes/student.js
@@ -4,18 +4,18 @@ const isAdmin = require('../utils/isAdmin');
 const { getStudents, getStudentById, getStudentsByCohorteId, getStudentsByGrouppmId, getStudentByUserId } = require('../controllers/student')
 
 //consultar por todos los alumnos
-server.get("/", getStudents)
+server.get("/", authenticate, getStudents)
 
 //consultar por la información de un alumno
-server.get("/:id", getStudentById)
+server.get("/:id", authenticate, getStudentById)
 
 //consultar por los alumnos de determinado cohorte
-server.get("/cohorte/:id", getStudentsByCohorteId)
+server.get("/cohorte/:id", authenticate, getStudentsByCohorteId)
 
 //consultar por los alumnos de un grupo pm
-server.get("/group-pm/:id", getStudentsByGrouppmId)
+server.get("/group-pm/:id", authenticate, getStudentsByGrouppmId)
 
 //consultar info de student por userId 
-server.get("/info/:id", getStudentByUserId)
+server.get("/info/:id", authenticate, getStudentByUserId)
 
 module.exports = server;
