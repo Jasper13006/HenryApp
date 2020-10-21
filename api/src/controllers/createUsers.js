e4143e74cd6253505e046f63ef5889325b2a440b
Manuel <manuelejdelatorre@gmail.com>
2020-10-21 12:49:04 -0300

    My message

--- api/src/controllers/createUsers.js
+++ api/src/controllers/createUsers.js
@@ -3,7 +3,7 @@ const { User } = require('../db.js')
 module.exports = {
     async generateUsers() {
         let users = [];
-        for (let id = 1; id <= 150; id++) {
+        for (let id = 1; id <= 1; id++) {
             const name = faker.name.firstName();
             const lastName = faker.name.lastName();
             const email = faker.internet.email();
